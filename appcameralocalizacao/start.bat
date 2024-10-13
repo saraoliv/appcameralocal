325e91afd0016ce05da797ce0f477c5df65ae297
IsabellySilvaM <mariaisabellymachado@outlook.com>
2024-10-12 22:34:01 -0300

    Criançaõ do Expo Go

--- appcameralocalizacao/start.bat
+++
@@ -1,3 +0,0 @@
-
-cls
-npx expo start
---
+++ appcamerelocalizao/app/(tabs)/localizacao.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export default function appcamerelocalizao() {
+  return(
+    <View style={styles.container}>
+
+    </View>
+  );
+}
